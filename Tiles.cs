ab3682aa76e8c97d5e2bab16c8c77ca813d70c05
Daniel Montes <dmontesgil@gmail.com>
2017-03-02 09:47:46 -0500

    Version refactorizada :/

--- Tiles.cs
+++ Tiles.cs
@@ -1,39 +1,39 @@
 public class Tile
 {
-    private int n;
-    private int n2;
+    private int i;
+    private int j;
     private Tile up;
     private Tile down;
     private Tile right;
     private Tile left;
 
-    public class Tile (int n, int n2)
+    public Tile(int i, int j)
     {
-        this.n = n;
-        this.n2 = n2;
+        this.i = i;
+        this.j=j;
     }
 
-    public void Left (Tile tile)
+    public void SetLeft (Tile tile)
     {
         left = tile;
     }
 
-    public void Right (Tile tile)
+    public void SetRight (Tile tile)
     {
         right = tile;
     }
 
-    public void Up (Tile tile)
+    public void SetUp (Tile tile)
     {
         up = tile;
     }
 
-    public void Down (Tile tile)
+    public void SetDown (Tile tile)
     {
         down = tile;
     }
 
-    public bool Check ()
+    public bool CheckUpTile ()
     {
         return up != null;
     }
@@ -49,40 +49,52 @@ public class TileMap
     private Tile [][] tiles;
     public TileMap (int width, int height, int connections)
     {
+        int rightAndLeftConnections = 1;
+        int upAndDownConnections = 0;
+
         tiles = new Tile[width][height];
-        for (int i=0; i<width; i++)
-        {
-            for (int j=0; i<height; j++)
-            {
-                tiles[i][j] = new Tile (i, j);
-            }
-        }
-        int dx = 1;
-        int dy = 0;
+        Tile fillTiles = FillMatrix(tiles);
+
         for (int i=0; i<connections; i++)
         {
-            int x = Random.Range (0, width-1);
-            int y = Random.Range (0, height-1);
-            Tile initial = tiles[x, y];
-            Tile tile = tiles[x+dx, y+dy];
-            if (dx == 1)
-                initial.Right (tile);
-            else if (dy == 1)
-                initial.Up (tile);
-            if (dx == 0)
-                dx = 1;
+            int initialX = Random.Range (0, width-1);
+            int initialY = Random.Range (0, height-1);
+
+            Tile initial = fillTiles[initialX, initialY];
+            Tile conecction = fillTiles[initialX+rightAndLeftConnections, initialY+upAndDownConnections];
+
+            if (rightAndLeftConnections == 1)
+                initial.SetRight (conecction);
+            else if (upAndDownConnections == 1)
+                initial.SetUp (conecction);
+
+            if (rightAndLeftConnections == 0)
+                rightAndLeftConnections = 1;
             else
-                dx = 0;
-            if (dy == 0)
-                dy = 1;
+                rightAndLeftConnections = 0;
+
+            if (upAndDownConnections == 0)
+                upAndDownConnections = 1;
             else
-                dy = 0;
+                upAndDownConnections = 0;
         }
     }
 
     public Tile GetTile (int x, int y)
     {
         return tiles[x, y];
+    }
+
+    public Tile[][] FillMatrix(Tile tiles)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; i < height; j++)
+            {
+                tiles[i][j] = new Tile(i, j);
+            }
+        }
+        return tiles;
     }
 }
 
@@ -92,7 +104,7 @@ public class Player
     private int y;
     private TileMap map;
 
-    public class Player (TileMap map, int x, int y)
+    public Player (TileMap map, int x, int y)
     {
         this.map = map;
         this.x = x;
